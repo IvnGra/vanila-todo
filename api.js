2ae34facaf71718f086d49fac5f7fd89e2b46e68
ivan gra <ivan.gratsov@tptlive.ee>
2025-05-27 13:51:39 +0300

    removed extra

--- api.js
+++ api.js
@@ -1,31 +1,5 @@
 const API_URL = "https://demo2.z-bit.ee";
 
-export async function login(username, password) {
-  const res = await fetch(`${API_URL}/users/get-token`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ username, password }),
-  });
-  if (!res.ok) throw new Error("Login failed");
-  const { access_token } = await res.json();
-  localStorage.setItem("access_token", access_token);
-}
-
-export async function register(username, password, firstname = "", lastname = "") {
-  const res = await fetch(`${API_URL}/users`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ username, newPassword: password, firstname, lastname }),
-  });
-  if (!res.ok) throw new Error("Registration failed");
-  const { access_token } = await res.json();
-  localStorage.setItem("access_token", access_token);
-}
-
-export function logout() {
-  localStorage.removeItem("access_token");
-}
-
 export function getToken() {
   return localStorage.getItem("access_token");
 }
